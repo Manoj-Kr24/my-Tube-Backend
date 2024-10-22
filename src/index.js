0fe71845b2d12ebd516ff02afc058874f0ca73a2
Manoj-Kr24 <kumarmanojmanjhi2406@gmail.com>
2024-10-22 10:35:14 +0530

    added some utils files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
+import { app } from "./app.js";
+
 dotenv.config({
     path:'./env'
 })
@@ -10,3 +12,18 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    const port = process.env.PORT || 8000;
+
+    app.on("error",(error)=>{
+        console.log("ERR :",error);
+        throw error
+    })
+    
+    app.listen(port, ()=>{
+        console.log(`Server is running at port : ${port}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection failed !! :: ",err)
+})
---
+++ src/utils/ApiError.js
@@ -0,0 +1,29 @@
+
+class ApiError extends Error{
+
+    constructor(
+        StatusCode,
+        message="Something went wrong",
+        errors=[],
+        statck=""
+    ){
+        super(message);
+        this.StatusCode=StatusCode;
+        this.data =null;
+        this.message =message;
+        this.success= false;
+        this.errors=errors
+
+        if(statck){
+            this.stack =statck;
+
+        }else{
+            Error.captureStackTrace(this,this.constructor);
+        }
+
+    }
+}
+
+
+
+export {ApiError}
