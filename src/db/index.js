d8161e1f47ff156753a320c75546c5053245f199
Manoj-Kr24 <kumarmanojmanjhi2406@gmail.com>
2024-10-22 07:48:43 +0530

    Established connection with  mongoDB atlas

---
+++ src/db/index.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+import {DB_NAME} from "../constants.js"
+
+
+const connectDB= async ()=>{
+
+    try {
+        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        console.log(`\n MongoBD connectecd !! DB Host ${connectionInstance.connection.host} :: PORT :${connectionInstance.connection.port} :: name : ${connectionInstance.connection.name}`)
+        console.log(`Ready state: ${connectionInstance.connection.readyState}`);
+
+        
+    } catch (error) {
+        console.log("Mongo DB connection FAILED :: ",error.message);
+        
+        process.exit(1)
+        
+    }
+    
+    
+}
+
+
+export default connectDB
+
+
+
